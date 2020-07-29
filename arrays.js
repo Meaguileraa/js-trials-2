91058eacf183a14bdb02e1d4204b0aedcba96e6c
Marissa Aguilera <marissaguilera@yahoo.com>
2020-07-28 17:23:49 -0700

    Arrays.js Complete

--- arrays.js
+++ arrays.js
@@ -3,17 +3,39 @@
 
 // 1. printIndices
 function printIndices(items) {
-  // Replace this with your code
+  for (const item in items)
+    console.log(`${items[item]} ${item}`);
 }
+printIndices(['apple', 'berry', 'cherry'])
 
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
-  // Replace this with your code
+    const result = [];
+
+    for (const i in items) {
+        if (i % 2 === 0) {
+            result.push(items[i]);
+        }    
+    }
+    return result;
 }
+everyOtherItem(['books', 'pencil', 'pen'])
+
+
 
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
-  // Replace this with your code
+
+    const sortedItems = items.sort((a, b) => a - b);
+    const sortedNItems = sortedItems.slice(0, n);
+
+    sortedNItems.reverse();
+    return sortedNItems
 }
+console.log(smallestNItems([1, 14, 3, 7, 0, 2], 3)); //[2, 1, 0]
+
+
+
+
