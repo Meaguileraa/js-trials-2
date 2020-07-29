eb553990554760c4aa5e15d8e145440c03d8a855
Marissa Aguilera <marissaguilera@yahoo.com>
2020-07-28 17:44:28 -0700

    Functions.js Complete

--- functions.js
+++ functions.js
@@ -3,14 +3,52 @@
 
 // 1. isHometown
 
-// Define your function here
+function isHometown(town) {
+
+    return town === 'San Francisco';
+}
+console.log(isHometown('Oakland')) //false 
+console.log(isHometown('San Francisco')) //true
 
 
 // 2. getFullName
 
-// Define your function here
+function getFullName(first, last) {
+
+    return (`${first} ${last}`);
+}
+console.log(getFullName('Marissa', 'Aguilera'))
+
 
 
 // 3. calculateTotal
+
+
+function calculateTotal(price, state, tax=0.05) {
+    const subtotal = price * (1 + tax); 
+    let fee = 0;
+
+    if (state === 'CA') {
+        fee = 0.03 * subtotal;
+    }
+    else if (state === 'PA') {
+        fee = 2;
+    }
+    else if (state === 'MA') {
+        if (price <= 100) {
+            fee = 1;
+        }
+        else {
+            fee = 3; 
+        }
+    }
+    return subtotal + fee; 
+}
+
+console.log(calculateTotal(40, 'CA'))
+console.log(calculateTotal(40, 'PA'))
+console.log(calculateTotal(40, 'MA'))
+
+
 
 // Define your function here
