eabd1512fd85224fd717dcab4b8bb59651cf716f
Marissa Aguilera <marissaguilera@yahoo.com>
2020-07-28 18:42:55 -0700

    Objects.js Completed

--- objects.js
+++ objects.js
@@ -3,11 +3,36 @@
 
 // 1. countWords
 function countWords(phrase) {
-  // Replace this with your code
+    const wordCount = {};
+
+    for (const word of phrase.split(' ')) {
+        if (wordCount.hasOwnProperty(word)) {
+            wordCount[word] += 1;
+        }
+        else {
+            wordCount[word] = 1;
+        }
+    }
+    return wordCount;
 }
+console.log(countWords('Hackbright is awesome and is also great'))
 
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
-  // Replace this with your code
-}
+
+    const melonPrice = {
+        2.50: ['Cantaloupe', 'Honeydew'], 
+        2.95: ['Watermelon'], 
+        3.25: ['Musk', 'Crenshaw'], 
+        14.95: ['Christmas']
+    };
+
+    if (!melonPrice[price]) {
+        return;
+    }
+
+    return melonPrice[price].sort();
+};
+
+console.log(getMelonsAtPrice(2.50));
